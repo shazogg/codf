f0a315a1d26b79c0d978418c879bee86ed61d05e
Shawn Franceus <shawn.franceus@gmail.com>
2021-01-22 18:56:10 +0100

    Add collab

--- app.js
+++ app.js
@@ -2,14 +2,31 @@
 
 // Constants
 const express = require("express");
+const express_ws = require("express-ws");
 const app = express();
-const port = 80;
+const port = 3000;
+
+// Add websocket to express app
+express_ws(app);
 
 // Static serv
 app.use(express.static("public"));
 
+// Websocket app
+app.ws("/ws", function(ws, req) {
+    // On websocket message
+    ws.on("message", function(msg) {
+        console.log(msg);
+    });
+    
+    // On websocket close
+    ws.on("close", function(msg) {
+        console.log("Closed !");
+    });
+});
+
 // App listening
-app.listen(process.env.PORT || port, () => {
+app.listen(process.env.PORT || port, function() {
     console.log("App running !");
 });
   
